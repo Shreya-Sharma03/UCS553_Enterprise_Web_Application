c9a6c9edd25fd3f63bb9c9bc5522035384c7921c
Shreya-Sharma03 <shreyasharma3506@gmail.com>
2026-08-16 19:51:00 +0530

    Assignment 2

--- Q1_Food_Delivery/src/food/model/FoodOrder.java
+++ Q1_Food_Delivery/src/food/model/FoodOrder.java
@@ -1,1 +1,42 @@
-
+package food.model;
+public abstract class FoodOrder{
+    private int orderId;
+    private String customerName;
+    private double amount;
+    private static String restaurantName="Food Express";
+    private static int orderCount=0;
+    public FoodOrder(int orderId, String customerName, double amount){
+        this.orderId=orderId;
+        this.customerName=customerName;
+        this.amount=amount;
+        orderCount++;
+    }
+    public int getOrderId(){
+        return orderId;
+    }
+    public String getCustomerName(){
+        return customerName;
+    }
+    public double getAmount(){
+        return amount;
+    }
+    public void setOrderId(int orderId){
+        this.orderId=orderId;
+    }
+    public void setCustomerName(String customerName){
+        this.customerName=customerName;
+    }
+    public void setAmount(double amount){
+        this.amount=amount;
+    }
+    public static String getRestaurantName(){
+        return restaurantName;
+    }
+    public static void setRestaurantName(String restaurantName){
+        FoodOrder.restaurantName=restaurantName;
+    }
+    public static void displayTotalOrders(){
+        System.out.println("Total Orders Created: "+orderCount);
+    }
+    public abstract double calculateDeliveryCharge();
+}
