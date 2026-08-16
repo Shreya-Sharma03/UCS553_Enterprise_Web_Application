cd012aaf6e94e3ef6be814fdef7d557bc4f806e6
Shreya-Sharma03 <shreyasharma3506@gmail.com>
2026-08-16 21:45:23 +0530

    Assignment 2

---
+++ Assignment 2/Q2_Smart_Library/src/library/service/LibraryService.java
@@ -0,0 +1,18 @@
+package library.service;
+import library.model.LibraryResource;
+import library.model.Printable;
+public class LibraryService{
+    public void displayAllResources(LibraryResource[] resources){
+        for (LibraryResource resource:resources){
+            Printable printable=(Printable) resource;
+            printable.printDetails();
+        }
+    }
+    public double calculateTotalFine(LibraryResource[] resources, int[] overdueDays){
+        double totalFine=0;
+        for (int i=0; i<resources.length; i++){
+            totalFine+=resources[i].calculateFine(overdueDays[i]);
+        }
+        return totalFine;
+    }
+}
