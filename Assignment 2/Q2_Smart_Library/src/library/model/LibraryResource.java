7d73706eb636b999fb015a14519a65e0aced08f9
Shreya-Sharma03 <shreyasharma3506@gmail.com>
2026-08-16 21:37:14 +0530

    Assignment 2

---
+++ Assignment 2/Q2_Smart_Library/src/library/model/LibraryResource.java
@@ -0,0 +1,42 @@
+package library.model;
+public abstract class LibraryResource{
+    private int resourceId;
+    private String title;
+    private String author;
+    private static String libraryName="Smart Central Library";
+    private static int resourceCount=0;
+    public LibraryResource(int resourceId, String title, String author){
+        this.resourceId=resourceId;
+        this.title=title;
+        this.author=author;
+        resourceCount++;
+    }
+    public int getResourceId(){
+        return resourceId;
+    }
+    public String getTitle(){
+        return title;
+    }
+    public String getAuthor(){
+        return author;
+    }
+    public void setResourceId(int resourceId){
+        this.resourceId=resourceId;
+    }
+    public void setTitle(String title){
+        this.title=title;
+    }
+    public void setAuthor(String author){
+        this.author=author;
+    }
+    public static String getLibraryName(){
+        return libraryName;
+    }
+    public static void setLibraryName(String libraryName){
+        LibraryResource.libraryName=libraryName;
+    }
+    public static void displayTotalResources() {
+        System.out.println("Total Resources Created: "+resourceCount);
+    }
+    public abstract double calculateFine(int overdueDays);
+}
