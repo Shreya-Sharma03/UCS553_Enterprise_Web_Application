28a575037d323f6574188fcaa3faa538fa896cb5
Shreya-Sharma03 <shreyasharma3506@gmail.com>
2026-08-16 20:49:56 +0530

    Assignment 2

---
+++ Assignment 2/Q1_Food_Delivery/src/food/service/OrderService.java
@@ -0,0 +1,10 @@
+package food.service;
+import food.model.FoodOrder;
+import food.utility.OrderUtility;
+public class OrderService{
+    public void displayAllOrders(FoodOrder[] orders){
+        for (FoodOrder order:orders) {
+            OrderUtility.generateOrderSummary(order);
+        }
+    }
+}
